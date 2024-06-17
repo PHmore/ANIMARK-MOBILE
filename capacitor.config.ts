ca9f571ed33513ade5ff9fc2cbc4b9aef156ad4a
PHmore <patryckhenryck401@gmail.com>
2024-06-17 01:45:23 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ANIMARK-mobile',
+  webDir: 'www'
+};
+
+export default config;
