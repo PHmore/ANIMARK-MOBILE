15e975ce6437f0ce79f66306e48270542b10ba58
PHmore <patryckhenryck401@gmail.com>
2024-06-26 18:22:42 -0300

    Versão 1.0

--- src/app/animes/animes.page.ts
+++ src/app/animes/animes.page.ts
@@ -5,7 +5,6 @@ import { HttpClient, HttpClientModule, HttpHeaders, HttpParams } from '@angular/
 import { IonicModule, LoadingController, NavController, ToastController } from '@ionic/angular';
 import { Storage } from '@ionic/storage-angular';
 import { Usuario } from '../home/usuario.model';
-// import { Anime } from '../animes/animes.model';
 import { Router, NavigationEnd, RouterModule } from '@angular/router';
 
 @Component({
@@ -19,10 +18,12 @@ import { Router, NavigationEnd, RouterModule } from '@angular/router';
 
 export class AnimesPage implements OnInit {
   currentRoute: string = '';
-  // Se não estiver como public não acessará o usuário carregado pela outra página
   public usuario: Usuario = new Usuario();
   public animeList: any[] = [];
+  public filteredAnimeList: any[] = [];
   public pageNumber = 1;
+  animeNome = '';
+  private isLoading = false;
 
   constructor(
     public http: HttpClient,
@@ -39,53 +40,45 @@ export class AnimesPage implements OnInit {
     });
   }
 
-  // É necessário importar o usuário coisa a qual eu estava esquecendo
   async ngOnInit() {
     await this.storage.create();
     const registro = await this.storage.get('usuario');
 
     if (registro) {
       this.usuario = Object.assign(new Usuario(), registro);
-      this.loadAnimeList(this.pageNumber);
+      this.loadAnimeList();
     } else {
       this.controle_navegacao.navigateRoot('/home');
     }
   }
 
-  nextPage() {
-    this.pageNumber++;
-    this.loadAnimeList(this.pageNumber);
-  }
+  async loadAnimeList() {
+    if (this.isLoading) return; 
 
-  prevPage() {
-    if (this.pageNumber > 1) {
-      this.pageNumber--;
-      this.loadAnimeList(this.pageNumber);
-    }
-  }
-
-  async loadAnimeList(pageNumber: number) {
+    this.isLoading = true;
     const loading = await this.controle_carregamento.create({ message: 'Pesquisando...', duration: 60000 });
     await loading.present();
-    
+
     let params = new HttpParams();
-    params = params.append('page', pageNumber.toString());
-  
-    const http_headers = new HttpHeaders({
+    params = params.append('page', this.pageNumber.toString());
+
+    const httpHeaders = new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': `Token ${this.usuario.token}`
     });
-  
-    this.http.get('http://127.0.0.1:8000/animes/api/anime-list/', { headers: http_headers, params }).subscribe({
+
+    this.http.get('http://127.0.0.1:8000/animes/api/anime-list/', { headers: httpHeaders, params }).subscribe({
       next: async (resposta: any) => {
-        this.animeList = resposta.anime_data;
-        console.log("LISTA DE ANIMES RETORNADAS: ", this.animeList);
+        this.animeList = this.animeList.concat(resposta.anime_data);
+        this.filteredAnimeList = [...this.animeList];
         loading.dismiss();
+        this.isLoading = false;
       },
       error: async (erro: any) => {
         loading.dismiss();
+        this.isLoading = false;
         const mensagem = await this.controle_toast.create({
-          message: `Falha ao consultar tasks: ${erro.message}`,
+          message: `Falha ao consultar animes: ${erro.message}`,
           cssClass: 'ion-text-center',
           duration: 2000
         });
@@ -93,17 +86,18 @@ export class AnimesPage implements OnInit {
       }
     });
   }
-  
-  async criarTask(anime: object)
-  {
-    console.log("ANIME A SALVAR: ",anime);
-    
 
-    // Inicializa interface com efeito de carregamento
+  async loadNextPage(event: any) {
+    this.pageNumber++;
+    await this.loadAnimeList();
+    event.target.complete();
+  }
+  
+  async criarTask(anime: any) {
+    anime.in_list = true;
     const loading = await this.controle_carregamento.create({ message: 'Adicionando...', duration: 15000 });
     await loading.present();
 
-    // Define informações do cabeçalho da requisição
     const http_headers = new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': `Token ${this.usuario.token}`
@@ -113,17 +107,11 @@ export class AnimesPage implements OnInit {
       'http://127.0.0.1:8000/animes/api/anime-task-create/',
       anime,
       { headers: http_headers }
-      
-      // O subscribe define um observador que espera uma resposta ou um erro
     ).subscribe({
-      // Este lida com o próximo valor obtido pelo subscribe ou seja a resposta da solicitação
       next: async (resposta: any) => {
-        
         loading.dismiss();
-        this.loadAnimeList(this.pageNumber);
-        // this.controle_navegacao.navigateRoot('/to-do-list');
+        this.updateAnimeList(anime);
       },
-      // Este lida com erros
       error: async (erro: any) => {
         loading.dismiss();
         const mensagem = await this.controle_toast.create({
@@ -133,8 +121,54 @@ export class AnimesPage implements OnInit {
         });
         mensagem.present();
       }
-      });
+    });
   }
+
+  updateAnimeList(anime: any) {
+    const index = this.animeList.findIndex(a => a.mal_id === anime.mal_id);
+    if (index !== -1) {
+      this.animeList[index].in_list = true;
+      this.filteredAnimeList = [...this.animeList];
+    }
+  }
+
+  async searchAnimes() {
+    if (this.isLoading) return;
+
+    this.isLoading = true;
+    const loading = await this.controle_carregamento.create({ message: 'Pesquisando...', duration: 60000 });
+    await loading.present();
+
+    let params = new HttpParams()
+      .set('anime_nome', this.animeNome)
+      .set('page', this.pageNumber.toString());
+
+    const httpHeaders = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Token ${this.usuario.token}`
+    });
+
+    this.http.get('http://127.0.0.1:8000/animes/api/anime-src/', { headers: httpHeaders, params })
+      .subscribe({
+        next: async (resposta: any) => {
+          this.animeList = resposta.anime_data;  // Considerando que 'anime_data' é o array de animes retornado pela API
+          this.filteredAnimeList = [...this.animeList];
+          loading.dismiss();
+          this.isLoading = false;
+        },
+        error: async (erro: any) => {
+          loading.dismiss();
+          this.isLoading = false;
+          const mensagem = await this.controle_toast.create({
+            message: `Falha ao consultar animes: ${erro.message}`,
+            cssClass: 'ion-text-center',
+            duration: 2000
+          });
+          mensagem.present();
+        }
+      });
+  }
+
 
   goToAnimes() {
     this.controle_navegacao.navigateRoot('/animes');
@@ -144,10 +178,8 @@ export class AnimesPage implements OnInit {
     this.controle_navegacao.navigateRoot('/to-do-list');
   }
 
-  logout()
-  {
+  logout() {
     this.storage.remove('usuario');
     this.controle_navegacao.navigateRoot('/home');    
   }
-
 }
